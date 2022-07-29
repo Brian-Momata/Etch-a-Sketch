c7b82c52801afcf8e97be3a08e54a237a789f560
Brian-Momata <brianmomata@gmail.com>
2022-07-29 13:43:21 +0300

    Fix createGrid function to adjust the gridcell size in relation to container size

--- script.js
+++ script.js
@@ -1,27 +1,18 @@
-const container = document.getElementById("container");
-let rows = document.getElementsByClassName("gridRow");
-let cells = document.getElementsByClassName("cell");
+let container = document.querySelector(".container");
+let size = parseInt(window.getComputedStyle(container).width, 10);
 
-
-generateGrid(16);
-
-function generateGrid(num) {
-    makeRows(num);
-    makeColumns(num);
+function createGrid(gridSize) {
+  for (let rowCol = 0; rowCol < gridSize ** 2; rowCol++) {
+    let gridCell = document.createElement("div");
+    Object.assign(gridCell.style, {
+      height : `${(size / gridSize) - 2}px`,
+      width : `${(size / gridSize) - 2}px`
+    });
+    gridCell.classList.add("cell");
+    container.appendChild(gridCell);
+  }
 }
 
-function makeRows(rowNum) {
-    for (r = 0; r < rowNum; r++) {
-        let row = document.createElement("div");
-        container.appendChild(row).className = "gridRow";
-    };
-};
-function makeColumns(cellNum) {
-    for (i = 0; i < rows.length; i++) {
-        for (j = 0; j < cellNum; j++) {
-            let newCell = document.createElement("div");
-            rows[j].appendChild(newCell).className = "cell";
-        };
-
-    };
-};
+window.onload = function() {
+  createGrid(32);
+}
